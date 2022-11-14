f0e641e926c5c6114438a0a2a34a304f7876343a
Sujal Valera <94304076+FLICKTHECODER@users.noreply.github.com>
2022-11-14 14:24:12 +0530

    Add files via upload

---
+++ phapi.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+
+
+app = Flask(__name__)
+
+@app.route("/bro")
+def bruh():
+  return "Hello"
+
+@app.route('/ph')
+def fetch_vidlink():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("https://www.saveporn.net/view_video.php?viewkey=ph602d29b697b36")
+    driver.implicitly_wait(1)
+    vid_480_btn = driver.find_element(By.XPATH, "//*[@id='dtable']/table/tbody/tr[2]/td[3]/a").click()
+    driver.implicitly_wait(5)
+    file = driver.window_handles[1]
+    driver.switch_to.window(file)
+    driver.implicitly_wait(3)
+    click_to_dl_btn = driver.find_element(By.XPATH, "//*[@id='sddlbtn']").click()
+    time.sleep(6)
+    return(f'{driver.current_url}')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
