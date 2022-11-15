b2b1dc79b73eaab619b449ebbb54fe226b23532d
Sujal Valera <94304076+FLICKTHECODER@users.noreply.github.com>
2022-11-15 21:58:48 +0530

    Update phapi.py

--- phapi.py
+++ phapi.py
@@ -12,24 +12,29 @@ app = Flask(__name__)
 def bruh():
   return "Hello"
 
-@app.route('/ph')
-def fetch_vidlink():
+@app.route('/ph/<vid_url>')
+def fetch_vidlink(vid_url):
+    if "pornhub.com" in vid_url:
+        vid_main_url = vid_url.replace("pornhub.com", "saveporn.net")
+        print(vid_main_url)
+
+    else:
+        return "Not a valid url :("
+
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    driver.get("https://www.saveporn.net/view_video.php?viewkey=ph602d29b697b36")
-    driver.implicitly_wait(1)
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(vid_main_url)
+    driver.implicitly_wait(2)
     vid_480_btn = driver.find_element(By.XPATH, "//*[@id='dtable']/table/tbody/tr[2]/td[3]/a").click()
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(3)
     file = driver.window_handles[1]
     driver.switch_to.window(file)
-    driver.implicitly_wait(3)
+    driver.implicitly_wait(1)
     click_to_dl_btn = driver.find_element(By.XPATH, "//*[@id='sddlbtn']").click()
-    driver.implicitly_wait(6)
+    driver.implicitly_wait(7)
     return(f'{driver.current_url}')
   
 if __name__ == "__main__":
