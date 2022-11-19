2df89a05174e02a760102ebf67d62f0c4529fe09
Sujal Valera <94304076+FLICKTHECODER@users.noreply.github.com>
2022-11-20 01:16:15 +0530

    Update phapi.py

--- phapi.py
+++ phapi.py
@@ -25,12 +25,10 @@ def fetch_vidlink(key):
     driver.get(vid_main_url)
     driver.implicitly_wait(2)
     vid_480_btn = driver.find_element(By.XPATH, "//*[@id='dtable']/table/tbody/tr[2]/td[3]/a").click()
-    driver.implicitly_wait(3)
+    driver.implicitly_wait(1)
     file = driver.window_handles[1]
     driver.switch_to.window(file)
     driver.implicitly_wait(1)
-    click_to_dl_btn = driver.find_element(By.XPATH, "//*[@id='sddlbtn']").click()
-    driver.implicitly_wait(8)
     return(f'{driver.current_url}')
   
 if __name__ == "__main__":
